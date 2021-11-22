ff47870b1fee9e5b6ffb67664f11ec031cbb1c29
jungle379 <jungle379@gmail.com>
2021-11-22 22:27:07 +0900

    🔥

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx}"],
-  darkMode: false, // 'media' or 'class'
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
-};
