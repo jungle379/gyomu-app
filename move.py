28a4e77ae2069f01b4ea9149cd753a4fdecba1fb
jungle379 <jungle379@gmail.com>
2021-12-10 21:33:16 +0900

    ⚡️

--- move.py
+++ move.py
@@ -3,14 +3,13 @@ import glob
 import shutil
 
 def file_move(move_path):
-  for x in glob.iglob('C:\\移動元パス\\**', recursive=True):
+  for x in glob.iglob('C:\\Users\morinohiroki\Desktop\\**', recursive=True):
     for y in glob.iglob(move_path+'**', recursive=True):
       if os.path.isfile(x) and x[x.rfind('\\')+1:x.rfind(' 第')] == y[y.rfind('\\')+1:y.rfind(' 第')]:
         shutil.move(x, move_path+x[x.rfind('\\')+1:])
         print(x, 'を 【', move_path, '】へ移動しました')
 
-file_move('C:\\移動先パス1\\')
-file_move('C:\\移動先パス2\\')
+file_move('\\Users\morinohiroki\Desktop\\')
 
 input(
 '''
--- rename.py
+++ rename.py
@@ -1,24 +1,59 @@
 import os
 import pandas as pd
 import glob
+import schedule
+from time import sleep
+from datetime import datetime,date
+import shutil
 
-# pandasにてCSVファイルからFAX番号を抽出
-df = pd.read_csv('/Users/morinohiroki/Desktop/test/sample.csv', sep=',', dtype=object)
-list_sample = df['FAX'].to_list()
-print(list_sample)
+# ここから実行内容
 
-# glob.globで指定のフォルダ内のファイル名を抽出、basenameにて拡張子を外す
-for f in glob.glob('/Users/morinohiroki/Desktop/test/*.jpg'):
-    # 拡張子の切り取り
-    basename_without_ext = os.path.splitext(os.path.basename(f))[0]
-    b = basename_without_ext[0:10]
-    print(b)
+def task():
+    # pandasにてCSVファイルからFAX番号を抽出
+    df = pd.read_csv('//LANDISK-A52D12/share/scanfile/before/sample.csv', sep=',', dtype=object)
+    list_sample = df['FAX'].to_list()
+    print(list_sample)
 
-# if A in B にてB内にAが含まれているか確認。合致したものについては担当の名前を追記
-# 変数後の[]にて、FAX番号のみ文字列にて抽出
-if basename_without_ext[1:10] in list_sample:
-    print('true')
-    path = '/Users/morinohiroki/Desktop/test/'
-    os.rename(f,os.path.join(path,basename_without_ext + "_山田.pdf"))
-else:
-    print('一覧とFAX番号が一致しませんでした。')
+    # glob.globで指定のフォルダ内のファイル名を抽出、basenameにて拡張子を外す
+    for f in glob.glob('//LANDISK-A52D12/share/scanfile/before/*.pdf'):
+        # 拡張子の切り取り
+        basename_without_ext = os.path.splitext(os.path.basename(f))[0]
+        b = basename_without_ext[0:10]
+
+        # if A in B にてB内にAが含まれているか確認。合致したものについては担当の名前を追記
+        # 変数後の[]にて、FAX番号のみ文字列にて抽出
+        if b in list_sample:
+            print('true')
+            path = '//LANDISK-A52D12/share/scanfile/before/'
+            os.rename(f,os.path.join(path,"中村_" + basename_without_ext + ".pdf" ))
+        else:
+            print('一覧とFAX番号が一致しませんでした。')
+
+    # 「名前_」を抽出してフォルダ間を移動させる
+    directory_pdf = "//LANDISK-A52D12/share/scanfile/before/中村_*.pdf"
+    for path in glob.glob(directory_pdf):
+        print( path )
+        
+    sourcepath='//LANDISK-A52D12/share/scanfile/before/'
+    source = os.listdir('//LANDISK-A52D12/share/scanfile/before/')
+    destinationpath = '//LANDISK-A52D12/share/scanfile/after/'
+    for files in source:
+        if files.startswith('中村_'):
+            shutil.move(os.path.join(sourcepath,files), os.path.join(destinationpath,files))
+
+# ここまで自動で起動する内容
+
+# 周期の設定値
+schedule.every(30).seconds.do(task)
+
+year = date.today().year
+month = date.today().month
+hour = 19
+minute = 0
+second = 0
+set_until_time = datetime(year,month,date.today().day,hour,minute,second)
+
+while datetime.now() < set_until_time:
+    schedule.run_pending()
+    sleep(1)
+
