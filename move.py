40989905764eacf99270645f5a9efe0f8a4b310c
jungle379 <63592531+jungle379@users.noreply.github.com>
2021-12-15 14:40:45 +0900

    Update move.py

--- move.py
+++ move.py
@@ -10,12 +10,12 @@ import shutil
 
 def task():
     # pandasにてCSVファイルからFAX番号を抽出
-    df = pd.read_csv('//LANDISK-A52D12/share/scanfile/before/sample.csv', sep=',', dtype=object)
+    df = pd.read_csv('//***/**/scanfile/before/sample.csv', sep=',', dtype=object)
     list_sample = df['FAX'].to_list()
     # print(list_sample)
 
     # glob.globで指定のフォルダ内のファイル名を抽出、basenameにて拡張子を外す
-    for f in glob.glob('//LANDISK-A52D12/share/scanfile/before/*.pdf'):
+    for f in glob.glob('//***/**/scanfile/before/*.pdf'):
         # 拡張子の切り取り
         basename_without_ext = os.path.splitext(os.path.basename(f))[0]
         b = basename_without_ext[0:10]
@@ -24,19 +24,19 @@ def task():
         # 変数後の[]にて、FAX番号のみ文字列にて抽出
         if b in list_sample:
             print('true')
-            path = '//LANDISK-A52D12/share/scanfile/before/'
+            path = '//***/**/scanfile/before/'
             os.rename(f,os.path.join(path,"山田_" + basename_without_ext + ".pdf" ))
         else:
             print('一覧とFAX番号が一致しませんでした。')
 
     # 「名前_」を抽出してフォルダ間を移動させる
-    directory_pdf = "//LANDISK-A52D12/share/scanfile/before/山田_*.pdf"
+    directory_pdf = "//***/**/scanfile/before/山田_*.pdf"
     for path in glob.glob(directory_pdf):
         print( path )
         
-    sourcepath='//LANDISK-A52D12/share/scanfile/before/'
-    source = os.listdir('//LANDISK-A52D12/share/scanfile/before/')
-    destinationpath = '//LANDISK-A52D12/share/scanfile/after/'
+    sourcepath='//***/**/scanfile/before/'
+    source = os.listdir('//***/**/scanfile/before/')
+    destinationpath = '//***/**/scanfile/after/'
     
     for files in source:
         if files.startswith('山田_'):
